b77e03a69df709a3ae2a6daab17092e9879f7ad4
Shah Parshwa <flamekaizer800@gmail.com>
2026-02-19 17:15:35 +0530

    Update 20260219105852-password.js

--- src/database/migrations/20260219105852-password.js
+++ src/database/migrations/20260219105852-password.js
@@ -4,7 +4,7 @@ const { DataTypes } = require('sequelize');
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('users', 'password', {
+    await queryInterface.addColumn('Users', 'password', {
       type: DataTypes.STRING,
       allowNull: false,
       defaultValue: 'parshwa@10',
@@ -12,6 +12,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('users', 'password');
+    await queryInterface.removeColumn('Users', 'password');
   },
 };
